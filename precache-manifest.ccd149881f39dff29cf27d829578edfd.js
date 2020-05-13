44b38bc9e390728406a64c3b67c3047b209c131b
Tara Hughes <taraaelisabeth@Taras-Air.home>
2020-05-13 14:32:31 -0500

    Updates

---
+++ precache-manifest.ccd149881f39dff29cf27d829578edfd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5392fd83d36e169c4ee3cfc835ef6df3",
+    "url": "/taraloves/index.html"
+  },
+  {
+    "revision": "c497e920d1a78217356d",
+    "url": "/taraloves/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f19fcd8378958d733ff4",
+    "url": "/taraloves/static/js/2.4a6c6e77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/taraloves/static/js/2.4a6c6e77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c497e920d1a78217356d",
+    "url": "/taraloves/static/js/main.9b2cc7f7.chunk.js"
+  },
+  {
+    "revision": "c5d7fb7e193fb1fbb63b",
+    "url": "/taraloves/static/js/runtime-main.2570d48e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/taraloves/static/media/logo.5d5d9eef.svg"
+  }
+]);
